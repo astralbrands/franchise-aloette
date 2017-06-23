7b387e0410946995c36279cd0ab5ce02af7581fc
Vincent Guidry <vguidry@tinroofsoftware.com>
2017-06-23 14:25:41 -0400

    form changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.8.1'
 
 set :application, 'franchise_aloette'
 set :repo_url, 'git@github.com:astralbrands/franchise-aloette.git'
